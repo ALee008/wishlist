9b1fc51bf047bf9fd9e1718e6d4135e6b6baf839
ALee008 <nomansland008@googlemail.com>
2019-01-11 23:23:29 +0100

    Create test.py
    Testscript

---
+++ test.py
@@ -0,0 +1,33 @@
+import time
+import functools
+from wishlist.core import Wishlist
+
+def clock(func):
+    @functools.wraps(func)
+    def clocked(*args, **kwargs):
+        t0 = time.time()
+        result = func(*args, **kwargs)
+        elapsed = time.time() - t0
+        name = func.__name__
+        arg_lst = []
+        if args:
+            arg_lst.append(', '.join(repr(arg) for arg in args))
+        if kwargs:
+            pairs = ['%s=%r' % (k, w) for k, w in sorted(kwargs.items())]
+            arg_lst.append(', '.join(pairs))
+        arg_str = ', '.join(arg_lst)
+        print('[%0.8fs] %s(%s) -> %r ' % (elapsed, name, arg_str, result))
+        return result
+    return clocked
+
+@clock
+def test():
+    name = "3ATVLKBO1V2CC"
+    w = Wishlist(name)
+    i = 0
+    for item in w:
+        print(item.jsonable())
+        i += 1
+    print('# items', i)
+
+test()
