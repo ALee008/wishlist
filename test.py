415192fc5fe668388bdf2a1320c48f8b9cd43b45
ALee008 <nomansland008@googlemail.com>
2019-03-20 21:58:57 +0100

    - test json - test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import glob
 import functools
@@ -31,15 +32,11 @@ def test(key):
     name = key
     w = Wishlist(name)
     res = []
-    i = 0
     for item in w:
         wishlist_offers = item.wishlist_offers.jsonable()
         res.append(wishlist_offers)
-        i += 1
-        if i == 1:
-            break
-    print(res)
-    print('# items', i)
+        #break
+    #print(res)
     return res
 
 
@@ -58,7 +55,7 @@ def test_offers():
 #test_offers()
 key = "3ATVLKBO1V2CC"
 
-#test(key)
+#new_wishlist_items = test(key)
 
 # list_of_wishlist_items = [{'Sabrent USB Externe Soundkarte für Windows und Mac. External Sound Card Stereo Adapter for Windows und Mac. Plug and Play. Keine Treiber erforderlich. (AU-MMSA)':
 #         {'title': 'Sabrent USB Externe Soundkarte für Windows und Mac. External Sound Card Stereo Adapter for Windows und Mac. Plug and Play. Keine Treiber erforderlich. (AU-MMSA)', 'price': 5.99, 'item_used_an_new_offers': '2', 'comment': '', 'added': 'Januar 26, 2019', 'rating': 4.3, 'digital': False, 'source': 'marketplace', 'lowest_nauop': OrderedDict([('seller', 'SLJ Trading'), ('price', '4.99'), ('shipping', '0.0'), ('condition', 'Neu')]), 'lowser_nauop_incl_shipping': OrderedDict([('seller', 'SLJ Trading'), ('price', '4.99'), ('shipping', '0.0'), ('condition', 'Neu')])}},
@@ -67,10 +64,42 @@ key = "3ATVLKBO1V2CC"
 #        {'Simba 107203950 - Squap Fangballspiel 2-er Set, 2-Sortiert, Mehrfarbig':
 #             {'title': 'Simba 107203950 - Squap Fangballspiel 2-er Set, 2-Sortiert, Mehrfarbig', 'price': 13.99, 'item_used_an_new_offers': '27', 'comment': '', 'added': 'April 15, 2018', 'rating': 4.3, 'digital': False, 'source': 'marketplace', 'lowest_nauop': OrderedDict([('seller', 'Jumpstore Megastore Online'), ('price', '12.48'), ('shipping', '13.50'), ('condition', 'Neu')]), 'lowser_nauop_incl_shipping': OrderedDict([('seller', 'Amazon.de'), ('price', '13.99'), ('shipping', '0.0'), ('condition', 'Neu')])}}]
 
-new_wishlist_items = [{'title': 'Sabrent USB Externe Soundkarte', 'price': 49.99, 'item_used_an_new_offers': '2', 'comment': '', 'added': 'Januar 26, 2019', 'rating': 4.3, 'digital': False, 'source': 'marketplace', 'lowest_nauop': OrderedDict([('seller', 'SLJ Trading'), ('price', '4.99'), ('shipping', '0.0'), ('condition', 'Neu')]), 'lowest_nauop_incl_shipping': OrderedDict([('seller', 'SLJ Trading'), ('price', '95.99'), ('shipping', '0.0'), ('condition', 'Neu')])},
-       {'title': 'Neuftech USB RFID Reader ID Kartenlesegerät Kartenleser Kontaktlos Card Reader für EM4100', 'price': 11.49, 'item_used_an_new_offers': '4', 'comment': '', 'added': 'Januar 26, 2019', 'rating': 4.1, 'digital': False, 'source': 'marketplace', 'lowest_nauop': OrderedDict([('seller', 'Aznoi'), ('price', '11.49'), ('shipping', '0.0'), ('condition', 'Neu')]), 'lowest_nauop_incl_shipping': OrderedDict([('seller', 'Aznoi'), ('price', '11.49'), ('shipping', '0.0'), ('condition', 'Neu')])},
-       {'title': 'Simba 107203950 - Squap Fangballspiel 2-er Set, 2-Sortiert, Mehrfarbig', 'price': 13.99, 'item_used_an_new_offers': '27', 'comment': '', 'added': 'April 15, 2018', 'rating': 4.3, 'digital': False, 'source': 'marketplace', 'lowest_nauop': OrderedDict([('seller', 'Jumpstore Megastore Online'), ('price', '12.48'), ('shipping', '13.50'), ('condition', 'Neu')]), 'lowest_nauop_incl_shipping': OrderedDict([('seller', 'Amazon.de'), ('price', '13.99'), ('shipping', '0.0'), ('condition', 'Neu')])}]
+# new_wishlist_items = [{'title': 'Sabrent USB Externe Soundkarte', 'price': 77.77, 'item_used_an_new_offers': '2', 'comment': '', 'added': 'Januar 26, 2019', 'rating': 4.3, 'digital': False, 'source': 'marketplace', 'lowest_nauop': OrderedDict([('seller', 'SLJ Trading'), ('price', '4.99'), ('shipping', '0.0'), ('condition', 'Neu')]), 'lowest_nauop_incl_shipping': OrderedDict([('seller', 'SLJ Trading'), ('price', '95.99'), ('shipping', '0.0'), ('condition', 'Neu')])},
+#        {'title': 'Neuftech USB RFID Reader ID Kartenlesegerät Kartenleser Kontaktlos Card Reader für EM4100', 'price': 11.49, 'item_used_an_new_offers': '4', 'comment': '', 'added': 'Januar 26, 2019', 'rating': 4.1, 'digital': False, 'source': 'marketplace', 'lowest_nauop': OrderedDict([('seller', 'Aznoi'), ('price', '11.49'), ('shipping', '0.0'), ('condition', 'Neu')]), 'lowest_nauop_incl_shipping': OrderedDict([('seller', 'Aznoi'), ('price', '11.49'), ('shipping', '0.0'), ('condition', 'Neu')])},
+#        {'title': 'Simba 107203950 - Squap Fangballspiel 2-er Set, 2-Sortiert, Mehrfarbig', 'price': 13.99, 'item_used_an_new_offers': '27', 'comment': '', 'added': 'April 15, 2018', 'rating': 4.3, 'digital': False, 'source': 'marketplace', 'lowest_nauop': OrderedDict([('seller', 'Jumpstore Megastore Online'), ('price', '12.48'), ('shipping', '13.50'), ('condition', 'Neu')]), 'lowest_nauop_incl_shipping': OrderedDict([('seller', 'Amazon.de'), ('price', '13.99'), ('shipping', '0.0'), ('condition', 'Neu')])}]
 
+# new_wishlist_items = [{'title': 'Neuftech USB RFID Reader ID Kartenlesegerät Kartenleser Kontaktlos Card Reader für EM4100', 'price': 11.49, 'item_used_an_new_offers': '4', 'comment': '', 'added': 'Januar 26, 2019', 'rating': 4.1, 'digital': False, 'source': 'marketplace', 'lowest_nauop': OrderedDict([('seller', 'Aznoi'), ('price', '11.49'), ('shipping', '0.0'), ('condition', 'Neu')]), 'lowest_nauop_incl_shipping': OrderedDict([('seller', 'Aznoi'), ('price', '11.49'), ('shipping', '0.0'), ('condition', 'Neu')])},
+#        {'title': 'Simba 107203950 - Squap Fangballspiel 2-er Set, 2-Sortiert, Mehrfarbig', 'price': 19.99, 'item_used_an_new_offers': '27', 'comment': '', 'added': 'April 15, 2018', 'rating': 4.3, 'digital': False, 'source': 'marketplace', 'lowest_nauop': OrderedDict([('seller', 'Jumpstore Megastore Online'), ('price', '12.48'), ('shipping', '13.50'), ('condition', 'Neu')]), 'lowest_nauop_incl_shipping': OrderedDict([('seller', 'Amazon.de'), ('price', '13.99'), ('shipping', '0.0'), ('condition', 'Neu')])},
+#                       {'title': 'Squap Fangballspiel 2-er Set, 2-Sortiert, Mehrfarbig',
+#                        'price': 9.99, 'item_used_an_new_offers': '27', 'comment': '', 'added': 'April 15, 2018',
+#                        'rating': 4.3, 'digital': False, 'source': 'marketplace', 'lowest_nauop': OrderedDict(
+#                           [('seller', 'Jumpstore Megastore Online'), ('price', '12.48'), ('shipping', '13.50'),
+#                            ('condition', 'Neu')]), 'lowest_nauop_incl_shipping': OrderedDict(
+#                           [('seller', 'Amazon.de'), ('price', '13.99'), ('shipping', '0.0'), ('condition', 'Neu')])}]
+
+new_wishlist_items = [
+    {
+        "title": "A History Of God",
+        "price": 10.86,
+        "item_used_an_new_offers": "36",
+        "comment": "",
+        "added": "M\u00e4rz 17, 2019",
+        "rating": 3.5,
+        "digital": False,
+        "source": "marketplace",
+        "lowest_nauop": {
+            "seller": "Bear Books Germany",
+            "price": "0.98",
+            "shipping": "8.20",
+            "condition": "Gebraucht - Gut"
+        },
+        "lowest_nauop_incl_shipping": {
+            "seller": "UNIVERSALBOOKSLTD",
+            "price": "0.77",
+            "shipping": "3.00",
+            "condition": "Gebraucht - Gut"
+        }
+    }, ]
 
 # com_data_structure:
 #
@@ -92,12 +121,13 @@ new_wishlist_items = [{'title': 'Sabrent USB Externe Soundkarte', 'price': 49.99
 
 export_file = r"testdata/jsons/export.json"
 #price_alert.write_json(new_wishlist_items, export_file)
-
+print("Finished writing to file.")
 def get_wishlist_item(list_with_dict, key):
     """
-
-    :param list_with_dict:
-    :return:
+    Return item name from list if exists (key).
+    :param list_with_dict: list with wish list item properties in dict.
+    :param key: item name
+    :return: wish list item if key matches, None else
     """
     try:
         res = [item for item in list_with_dict if item['title'] == key][0]
@@ -107,27 +137,26 @@ def get_wishlist_item(list_with_dict, key):
     return res
 
 # get keys
-dict_keys = [wishlist_item['title'] for wishlist_item in new_wishlist_items]
+dict_keys_latest_wishlist = [wishlist_item['title'] for wishlist_item in new_wishlist_items]
 old_wishlist_items = price_alert.read_json(export_file)
-for key in dict_keys:
-    old_wishlist_item_data = get_wishlist_item(old_wishlist_items, key)
-    new_wishlist_item_data = get_wishlist_item(new_wishlist_items, key)
+dict_keys_old_wishlist = [wishlist_item['title'] for wishlist_item in old_wishlist_items]
+
+added, removed = price_alert.items_added_or_removed(dict_keys_latest_wishlist, dict_keys_old_wishlist, True)
+
+for wishlist_item in dict_keys_latest_wishlist:
+    old_wishlist_item_data = get_wishlist_item(old_wishlist_items, wishlist_item)
+    new_wishlist_item_data = get_wishlist_item(new_wishlist_items, wishlist_item)
     if old_wishlist_item_data and new_wishlist_item_data:
-        alert = price_alert.PriceAlert(old_wishlist_item_data, new_wishlist_item_data)
+        alert = price_alert.PriceAlert(new_wishlist_item_data, old_wishlist_item_data, wishlist_item)
         amazon_prices = alert.price_changed
-        if amazon_prices:
-            print("Amazon price for \"{}\" changed from {:.2f},- to {:.2f},-. Price difference {:.2f}%.".format(
-                key, alert.old_item_price, alert.new_item_price, alert.relevative_price_difference
-            )
-                 )
-
         used_and_new_offer_prices = alert.nauo_price_changed
+
+try:
+    pushbullet_api_key = sys.argv[1]
+    if pushbullet_api_key:
+        if amazon_prices:
+            resp1 = price_alert.push_message("Amazon Price Alert", amazon_prices, pushbullet_api_key)
         if used_and_new_offer_prices:
-            print("New and used price offer for \"{}\" changed from {:.2f},- to {:.2f},-. Price difference {:.2f}%.".format(
-                key, alert.old_nauo_price, alert.new_nauo_price, alert.relevative_nauo_price_difference
-            )
-                 )
-
-
-# TODO: what if item is added to wishlist
-# TODO: what if item is removed from wishlist
+            resp2 = price_alert.push_message("New & Used Price Alert", used_and_new_offer_prices, pushbullet_api_key)
+except IndexError:
+    pass
